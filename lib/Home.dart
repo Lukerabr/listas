8456d384315a46b478df2bfe870571f9a8f8d1d1
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-08-31 23:14:30 -0400

    Teste listas.

---
+++ lib/Home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, indice){
+            print("item ${indice}");
+            return ListTile(
+              title: Text(indice.toString()),
+              subtitle: Text("subtitulo"),
+            );
+          }
+        ),
+      ),
+    );
+  }
+}
